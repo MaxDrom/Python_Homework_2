e740b983c27d44cecd594e35767ae947fe499eca
Marks <maxidrom310@gmail.com>
2022-12-02 07:17:04 +0300

    2.11 new

--- 2.11/prog.py
+++ 2.11/prog.py
@@ -3,6 +3,7 @@ class SuperList(list):
         types = {}
         types[float] = "float"
         types[int] = "int"
+        types[str] = "string"
         file = open(filename, 'w')
         file.writelines([f'{types[type(element)]} {element}\n' for element in self])
         file.close()
@@ -12,15 +13,17 @@ class SuperList(list):
         casts = {}
         casts["float"] = lambda x: float(x)
         casts["int"] = lambda x: int(x)
+        casts["string"] = lambda x: x
         file = open(filename, 'r')
         res = []
         for line in file:
-            (t, value) = line.split(" ")
+            t, *value = line.split(" ")
+            value = " ".join(value)[:-1]
             res.append(casts[t](value))
         file.close()
         return SuperList(res)
 
-a = SuperList([5,4,3, 10.0, 20.0])
+a = SuperList([5,4,3, 10.0, 20.0, "sd"])
 a.save("test1.txt")
 b = SuperList.load("test2.txt")
-print(*b)
+print(b)
